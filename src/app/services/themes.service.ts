578a245539d382f5a119ced685e292864588ed21
Esteban <esteban321duran@gmail.com>
2023-04-21 00:13:17 -0300

    Build 1.1 - assets file paths were incorrectly started with a '/' character.

--- src/app/services/themes.service.ts
+++ src/app/services/themes.service.ts
@@ -10,32 +10,32 @@ export class ThemesService {
     {
       name:'Atreides',
       CSSThemeClass:'atreides-theme-dark',
-      factionLogoSrc: '/assets/images/atreides-logo.svg'
+      factionLogoSrc: 'assets/images/atreides-logo.svg'
     },
     {
       name:'Harkonnen',
       CSSThemeClass:'harkonnen-theme-dark',
-      factionLogoSrc: '/assets/images/harkonnen-logo.svg'
+      factionLogoSrc: 'assets/images/harkonnen-logo.svg'
     },
     {
       name:'Fremen',
       CSSThemeClass:'fremen-theme-dark',
-      factionLogoSrc: '/assets/images/fremen-logo.svg'
+      factionLogoSrc: 'assets/images/fremen-logo.svg'
     },
     {
       name:'Emperor',
       CSSThemeClass:'emperor-theme-dark',
-      factionLogoSrc: '/assets/images/emperor-logo.svg'
+      factionLogoSrc: 'assets/images/emperor-logo.svg'
     },
     {
       name:'Guild',
       CSSThemeClass:'guild-theme-dark',
-      factionLogoSrc: '/assets/images/guild-logo.svg'
+      factionLogoSrc: 'assets/images/guild-logo.svg'
     },
     {
       name:'Bene-Gesserit',
       CSSThemeClass:'bene-gesserit-theme-dark',
-      factionLogoSrc: '/assets/images/bene-gesserit-logo.svg'
+      factionLogoSrc: 'assets/images/bene-gesserit-logo.svg'
     },
   ]
   getThemes(){
